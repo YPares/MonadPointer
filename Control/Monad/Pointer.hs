32f4f3e392b3d25635d44441f7cb2c77a79d8261
YPares <yves.pares@gmail.com>
2014-04-16 16:17:41 +0200

    Introduced the MTSet constaint type. Problem: requires UndecidableInstances

--- Control/Monad/Pointer.hs
+++ Control/Monad/Pointer.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE TypeOperators, KindSignatures, Rank2Types, FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving, MultiParamTypeClasses #-}
-{-# LANGUAGE OverlappingInstances, ScopedTypeVariables #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverlappingInstances #-}
+{-# LANGUAGE TypeFamilies, DataKinds, ConstraintKinds, UndecidableInstances #-}
+
 
 module Control.Monad.Pointer where
 
@@ -25,35 +27,51 @@ newtype (::>) (mt1 :: (* -> *) -> * -> *) (m2 :: * -> *) a = PendingStackCell (m
 infixr 6 ::>
 
 {-# INLINE stackFirst #-}
-stackFirst :: (mt1 ::> pstackrest) a -> mt1 pstackrest a
+stackFirst :: (mt1 ::> stackrest) a -> mt1 stackrest a
 stackFirst (PendingStackCell action) = action
 -}
 
 {-# INLINE (<::) #-}
-(<::) :: (mt1 :: (* -> *) -> * -> *) (pstackrest :: * -> *) a -> (mt1 pstackrest a -> c) -> c
+(<::) :: (mt1 :: (* -> *) -> * -> *) (stackrest :: * -> *) a -> (mt1 stackrest a -> c) -> c
 action <:: f = f action
 
 infixl 6 <::
 
 --class PointToBase 
 
-class PointableIn (pstack :: * -> *) (mttarget :: (* -> *) -> * -> *) where
-  mpoint :: (forall (m :: * -> *) . (Functor m, Applicative m, Monad m) => mttarget m a) -> pstack a
+class (Functor stack, Applicative stack, Monad stack)
+  => PointableIn (stack :: * -> *) (mttarget :: (* -> *) -> * -> *) where
+  mpoint :: (forall (m :: * -> *) . (Functor m, Applicative m, Monad m) => mttarget m a) -> stack a
 
-instance (Functor pstackrest, Applicative pstackrest, Monad pstackrest)
-         => PointableIn (mt1 pstackrest) mt1 where
+instance (Functor stackrest, Applicative stackrest, Monad stackrest,
+          Functor (mt1 stackrest), Applicative (mt1 stackrest), Monad (mt1 stackrest))
+         => PointableIn (mt1 stackrest) mt1 where
   {-# INLINE mpoint #-}
   mpoint action = action
 
-instance (PointableIn pstackrest mt2, MonadTrans mt1, Monad pstackrest)
-         => PointableIn (mt1 pstackrest) mt2 where
+instance (PointableIn stackrest mt2, MonadTrans mt1,
+          Functor stackrest, Applicative stackrest, Monad stackrest,
+          Functor (mt1 stackrest), Applicative (mt1 stackrest), Monad (mt1 stackrest))
+         => PointableIn (mt1 stackrest) mt2 where
   {-# INLINE mpoint #-}
   mpoint action = lift (mpoint action)
 
 type At mt a = (Functor m, Applicative m, Monad m) => mt m a
+-- ^ Prevents sharing of the m type!
 
 
-test :: StateT Int (ReaderT Double (ReaderT Double IO)) String
+type family MTSetGetConstraints
+            cst (stack :: * -> *)
+            (l :: [(* -> *) -> * -> *]) where
+  MTSetGetConstraints cst stack (mt ': rest) =
+                                 MTSetGetConstraints (cst, PointableIn stack mt) stack rest
+  MTSetGetConstraints cst stack '[] = cst
+
+type MTSet (l :: [(* -> *) -> * -> *]) (stack :: * -> *) =
+  (Functor stack, Applicative stack, Monad stack, MTSetGetConstraints () stack l)
+
+
+test :: (MTSet '[StateT Int, ReaderT Double] m, MonadIO m) => m String
 test = do x <- mpoint $ helper 42
           liftIO $ print x
           return (show (x::Double))
@@ -61,5 +79,5 @@ test = do x <- mpoint $ helper 42
 helper :: (Num t) => t -> At (ReaderT t) t
 helper x = (*x) <$> ask
 
-
-x = test <:: flip runStateT 3 <:: flip runReaderT 10 <:: flip runReaderT 1
+x :: IO (String, Int)
+x = test <:: flip runStateT (3::Int) <:: flip runReaderT (10::Double) <:: flip runReaderT (1::Double)
